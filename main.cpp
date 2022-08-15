2e71408e695971d3e0e8839971db8acf61ba4a49
Program <67320703+Program132@users.noreply.github.com>
2022-08-15 18:21:56 +0200

    Argument CMD -> donner le nom du fichier.

--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@
 #include <optional>
 #include <fstream>
 
-int main() {
+int main(int argc, char** argv) {
     std::string fileName;
-    std::cout << "Fichier : ";
-    std::cin >> fileName;
-    std::cout << std::endl;
-
+    if (argc == 2) {
+        fileName = argv[1];
+    } else {
+        std::cout << "Fichier : ";
+        std::cin >> fileName;
+        std::cout << std::endl;
+    }
     std::ifstream file { fileName};
+    if (!file) {
+        std::cerr << "Donnez le nom correct du fichier." << std::endl;
+        return 1;
+    }
     std::string f_content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
     FPL::TokenBuilding t;
     std::vector<FPL::Token> tokens = t.parseToken(f_content);
