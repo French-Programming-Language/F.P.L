a7caf1f22f24d567dfc59128d5fe915129b24050
Program <67320703+Program132@users.noreply.github.com>
2022-10-16 15:21:50 +0200

    Functions Convertions.

---
+++ src/Utils/Converts.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+#include <optional>
+#include <map>
+#include <functional>
+#include <fstream>
+
+int stringToInt(std::string value, std::string_view message);
+
+double stringToDouble(std::string value, std::string_view message);
