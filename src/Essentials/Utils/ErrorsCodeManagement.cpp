f0af713375aa699a4c4da7bfcaae7da6c73dfbd8
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-04-19 13:25:18 +0200

    refactor: creates a macro to avoid redundancy

--- src/Essentials/Utils/ErrorsCodeManagement.cpp
+++ src/Essentials/Utils/ErrorsCodeManagement.cpp
@@ -1,259 +1,48 @@
 #include "ErrorsCodeManagement.h"
 
-void forgotType(FPL::Data::Data& data) {
-    std::cerr << "Vous avez oublie de preciser le type, ligne "
-    << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(1);
-}
+#define CREATE_ERROR(functionName, error, exitType) \
+	void functionName(FPL::Data::Data& data) { std::cerr << error << data.current_token->TokenLineNumber << "." << std::endl; exit(exitType); } 
 
-void forgotName(FPL::Data::Data& data) {
-    std::cerr << "Vous avez oublie de donner un nom, ligne "
-    << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(2);
-}
-
-void forgotEgalOperators(FPL::Data::Data& data) {
-    std::cerr << "Vous avez oublie de placer les operateurs '->' pour donner une valeur, ligne "
-    << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(3);
-}
-
-void forgotValue(FPL::Data::Data& data) {
-    std::cerr << "Vous avez oublie de donner une valeur, ligne "
-    << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(4);
-}
-
-void forgotEndInstructionOperator(FPL::Data::Data& data) {
-    std::cerr << "Vous avez oublie de mettre l'operateur ';' pour mettre fin a l'instruction., ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(5);
-}
-
-void differentTypes(FPL::Data::Data& data) {
-    std::cerr << "Vous avez declarer deux types differents dans une meme expression ce qui n'est pas possible, ligne "
-    << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(6);
-}
-
-void variableAlreadyExist(FPL::Data::Data& data) {
-    std::cerr << "Vous avez deja declare une variable avec ce nom, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(7);
-}
-
-void variableDoesNotExist(FPL::Data::Data& data) {
-    std::cerr << "La variable que vous cherchez n'existe pas, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(8);
-}
-
-void BoolNotLikeValue(FPL::Data::Data& data) {
-    std::cerr << "Pour utiliser le type bool vous devez pas mettre entre '\"' votre valeur mais marquer 'vrai' ou 'faux' simplement, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(9);
-}
-
-void wrongTypeForBool(FPL::Data::Data& data) {
-    std::cerr << "Pour utiliser un type bool vous devez mettre en argument 'boolean' ou 'bool', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(10);
-}
-
-void needValueNextOperatorCondition(FPL::Data::Data& data) {
-    std::cerr << "Apres votre operateur conditionnelle vous devez specifier une valeur, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(11);
-}
-
-void inputTypeError(FPL::Data::Data& data) {
-    std::cerr << "Votre reponse a l'entree clavier comporte un type non similaire a votre type preciser, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(12);
-}
-
-void wrongType(FPL::Data::Data& data) {
-    std::cerr << "Le type de la valeur preciser n'est pas valide, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(13);
-}
-
-void CONVERSION_sameTypeVariable(FPL::Data::Data& data) {
-    std::cerr << "Le type precise est le meme que le type de votre variable dans votre conversion donc aucun changement n'est necessaire, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(14);
-}
-
-void CONVERSION_boolNumberFromInt(FPL::Data::Data& data) {
-    std::cerr << "La variable doit etre de type 'entier' (int) et doit contenir 1 pour 'vrai' et 0 pour 'faux', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(15);
-}
-
-void FONCTION_forgotfirstParenthese(FPL::Data::Data& data) {
-    std::cerr << "Vous devez ouvrir les parentheses pour inserer des arguments si vous le souhaitez, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(16);
-}
-
-void FONCTION_forgotargtype(FPL::Data::Data& data) {
-    std::cerr << "Vous devez donner un le type de votre argument, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(17);
-}
-
-void FONCTION_forgotargname(FPL::Data::Data& data) {
-    std::cerr << "Vous devez donner un le nom de votre argument, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(18);
-}
-
-void FONCTION_forgotaddarg(FPL::Data::Data& data) {
-    std::cerr << "Vous devez separer vos argument avec une ',', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(19);
-}
-
-void FONCTION_forgotinsertcode(FPL::Data::Data& data) {
-    std::cerr << "Vous devez ouvrir et fermer avec '{' et '}' pour inserer votre code, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(20);
-}
-
-void FONCTION_argumentexist(FPL::Data::Data& data) {
-    std::cerr << "L'argument existe deja, merci de donner un autre nom, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(21);
-}
-
-void FONCTION_forgotnametocall(FPL::Data::Data& data){
-    std::cerr << "Vous devez preciser le nom de la fonction que vous souhaitez appeler, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(22);
-}
-
-void FONCTION_forgotargumenttogivevalue(FPL::Data::Data& data) {
-    std::cerr << "Vous devez donner un argument, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(23);
-}
-
-void FONCTION_forgotargumentvalue(FPL::Data::Data& data) {
-    std::cerr << "Vous devez transmettre une valeur a votre argument, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(24);
-}
-
-void FONCTION_argumentDoesNotExist(FPL::Data::Data& data) {
-    std::cerr << "Cet argument n'existe pas, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(25);
-}
-
-void FONCTION_noneedargs(FPL::Data::Data& data) {
-    std::cerr << "La fonction ne demande pas des arguments donc veuillez retirer ':' et mettre directement ';' à la fin de l'instruction, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(26);
-}
-
-void FONCTION_didnotfindarg(FPL::Data::Data& data) {
-    std::cerr << "L'argument est introuvable, veuillez reessayer ou contacter l'equipe de developpement, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(27);
-}
-
-void VAR_wrongparameter(FPL::Data::Data& data) {
-    std::cerr << "Vous devez preciser un parametre pour votre variable qui existe : 'globale', 'fonction', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(28);
-}
-
-void VAR_closeparameter(FPL::Data::Data& data) {
-    std::cerr << "Vous devez fermer les parenthese si vous avez voulu introduire un parametre pour votre variable, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(29);
-}
-
-void FONCTION_doesnotexist(FPL::Data::Data& data) {
-    std::cerr << "La fonction n'existe pas, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(30);
-}
-
-void RETURN_noinfonction(FPL::Data::Data& data) {
-    std::cerr << "Vous ne devez pas utiliser 'renvoyer' en dehors d'une fonction, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(31);
-}
-
-void RETURN_novariablefound(FPL::Data::Data& data) {
-    std::cerr << "Vous devez declarer une variable sous ce format : 'variable TYPE_DE_RETOUR fonction NOM -> FONCTION;', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(32);
-}
-
-void RETURN_noreturn(FPL::Data::Data& data) {
-    std::cerr << "Aucune instruction 'renvoyer' n'a ete trouve, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(33);
-}
-
-void RETURN_wrongtype(FPL::Data::Data& data) {
-    std::cerr << "Les types doivent etre les meme et non differents, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(34);
-}
-
-void IMPORT_needfilename(FPL::Data::Data& data) {
-    std::cerr << "Vous devez donner le nom du fichier entre guillements : '\"', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(35);
-}
-
-void PAQUET_forgotname(FPL::Data::Data& data) {
-    std::cerr << "Vous devez donner a nom a votre paquet, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(36);
-}
-
-void PAQUET_open(FPL::Data::Data& data) {
-    std::cerr << "Vous devez ouvrir les '{' pour mettre votre code, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(37);
-}
-
-void PAQUET_close(FPL::Data::Data& data) {
-    std::cerr << "Vous devez fermer l'introduction du code avec '}' pour mettre votre code, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(38);
-}
-
-void FICHIER_fileunknow(FPL::Data::Data& data) {
-    std::cerr << "Fichier non trouve, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(39);
-}
-
-void FICHIER_varunknow(FPL::Data::Data& data) {
-    std::cerr << "Variable non trouve, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(40);
-}
-
-void FICHIER_wrongargument(FPL::Data::Data& data) {
-    std::cerr << "Mauvais argument pour utiliser 'fichier' en instruction : 'lire' ou 'ecrire', ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(41);
-}
-
-void FICHIER_stringtypetogetfile(FPL::Data::Data& data) {
-    std::cerr << "Pour recuperer le fichier vous devez le mettre entre '\"' donc de type 'texte' (string), ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(42);
-}
-
-void FICHIER_needvariable(FPL::Data::Data& data) {
-    std::cerr << "Precisez le nom d'une variable existante ou non, ligne "
-              << data.current_token->TokenLineNumber << "." << std::endl;
-    exit(42);
-}
+CREATE_ERROR(forgotType, "Vous avez oublie de preciser le type, ligne ", 1);
+CREATE_ERROR(forgotName, "Vous avez oublie de donner un nom, ligne ", 2);
+CREATE_ERROR(forgotEgalOperators, "Vous avez oublie de placer les operateurs '->' pour donner une valeur, ligne ", 3);
+CREATE_ERROR(forgotValue, "Vous avez oublie de donner une valeur, ligne ", 4);
+CREATE_ERROR(forgotEndInstructionOperator, "Vous avez oublie de mettre l'operateur ';' pour mettre fin a l'instruction, ligne ", 5);
+CREATE_ERROR(differentTypes, "Vous avez declare deux types differents dans une meme expression ce qui n'est pas possible, ligne ", 6);
+CREATE_ERROR(variableAlreadyExist, "Vous avez deja declare une variable avec ce nom, ligne ", 7);
+CREATE_ERROR(variableDoesNotExist, "La variable que vous cherchez n'existe pas, ligne ", 8);
+CREATE_ERROR(BoolNotLikeValue, "Pour utiliser le type bool vous devez pas mettre entre '\"' votre valeur mais marquer 'vrai' ou 'faux' simplement, ligne ", 9);
+CREATE_ERROR(wrongTypeForBool, "Pour utiliser un type bool vous devez mettre en argument 'boolean' ou 'bool', ligne ", 10);
+CREATE_ERROR(needValueNextOperatorCondition, "Apres votre operateur conditionnelle vous devez specifier une valeur, ligne ", 11);
+CREATE_ERROR(inputTypeError, "Votre reponse a l'entree clavier comporte un type non similaire a votre type preciser, ligne ", 12);
+CREATE_ERROR(wrongType, "Le type de la valeur preciser n'est pas valide, ligne ", 13);
+CREATE_ERROR(CONVERSION_sameTypeVariable, "Le type precise est le meme que le type de votre variable dans votre conversion donc aucun changement n'est necessaire, ligne ", 14);
+CREATE_ERROR(CONVERSION_boolNumberFromInt, "La variable doit etre de type 'entier' (int) et doit contenir 1 pour 'vrai' et 0 pour 'faux', ligne ", 15);
+CREATE_ERROR(FONCTION_forgotfirstParenthese, "Vous devez ouvrir les parentheses pour inserer des arguments si vous le souhaitez, ligne ", 16);
+CREATE_ERROR(FONCTION_forgotargtype, "Vous devez donner un le type de votre argument, ligne ", 17);
+CREATE_ERROR(FONCTION_forgotargname, "Vous devez donner un le nom de votre argument, ligne ", 18);
+CREATE_ERROR(FONCTION_forgotaddarg, "Vous devez separer vos argument avec une ',', ligne ", 19);
+CREATE_ERROR(FONCTION_forgotinsertcode, "Vous devez ouvrir et fermer avec '{' et '}' pour inserer votre code, ligne ", 20);
+CREATE_ERROR(FONCTION_argumentexist, "L'argument existe deja, merci de donner un autre nom, ligne ", 21);
+CREATE_ERROR(FONCTION_forgotnametocall, "Vous devez preciser le nom de la fonction que vous souhaitez appeler, ligne ", 22);
+CREATE_ERROR(FONCTION_forgotargumenttogivevalue, "Vous devez donner un argument, ligne ", 23);
+CREATE_ERROR(FONCTION_forgotargumentvalue, "Vous devez transmettre une valeur a votre argument, ligne ", 24);
+CREATE_ERROR(FONCTION_argumentDoesNotExist, "Cet argument n'existe pas, ligne ", 25);
+CREATE_ERROR(FONCTION_noneedargs, "La fonction ne demande pas des arguments donc veuillez retirer ':' et mettre directement ';' a la fin de l'instruction, ligne ", 26);
+CREATE_ERROR(FONCTION_didnotfindarg, "L'argument est introuvable, veuillez reessayer ou contacter l'equipe de developpement, ligne ", 27);
+CREATE_ERROR(VAR_wrongparameter, "Vous devez preciser un parametre pour votre variable qui existe : 'globale', 'fonction', ligne ", 28);
+CREATE_ERROR(VAR_closeparameter, "Vous devez fermer les parenthese si vous avez voulu introduire un parametre pour votre variable, ligne ", 29);
+CREATE_ERROR(FONCTION_doesnotexist, "La fonction n'existe pas, ligne ", 30);
+CREATE_ERROR(RETURN_noinfonction, "Vous ne devez pas utiliser 'renvoyer' en dehors d'une fonction, ligne ", 31);
+CREATE_ERROR(RETURN_novariablefound, "Vous devez declarer une variable sous ce format : 'variable TYPE_DE_RETOUR fonction NOM -> FONCTION;', ligne ", 32);
+CREATE_ERROR(RETURN_noreturn, "Aucune instruction 'renvoyer' n'a ete trouve, ligne ", 33);
+CREATE_ERROR(RETURN_wrongtype, "Les types doivent etre les meme et non differents, ligne ", 34);
+CREATE_ERROR(IMPORT_needfilename, "Vous devez donner le nom du fichier entre guillements : '\"', ligne ", 35);
+CREATE_ERROR(PAQUET_forgotname, "Vous devez donner a nom a votre paquet, ligne ", 36);
+CREATE_ERROR(PAQUET_open, "Vous devez ouvrir les '{' pour mettre votre code, ligne ", 37);
+CREATE_ERROR(PAQUET_close, "Vous devez fermer l'introduction du code avec '}' pour mettre votre code, ligne ", 38);
+CREATE_ERROR(FICHIER_fileunknow, "Fichier non trouve, ligne ", 39);
+CREATE_ERROR(FICHIER_varunknow, "Variable non trouve, ligne ", 40)
+CREATE_ERROR(FICHIER_wrongargument, "Mauvais argument pour utiliser 'fichier' en instruction : 'lire' ou 'ecrire', ligne ", 41)
+CREATE_ERROR(FICHIER_stringtypetogetfile, "Pour recuperer le fichier vous devez le mettre entre '\"' donc de type 'texte' (string), ligne ", 42)
+CREATE_ERROR(FICHIER_needvariable, "Precisez le nom d'une variable existante ou non, ligne ", 42)
