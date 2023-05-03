014ac6d2a24d5e0c99091c43b25ea7143733d048
Program132 <clement.gauthier132@orange.fr>
2023-05-03 20:18:32 +0200

    Code Fonctions pour la boucle "tant que" dans le .cpp

---
+++ src/Instructions/TantQue_Loop.cpp
@@ -0,0 +1,160 @@
+#include "TantQue_Loop.h"
+
+namespace FPL::Instruction::TantQue {
+    void increment_int_operatorLowerUpper(VariableDef& var, int value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        int variable_v = stringToInt(var.VariableValue, "");
+
+        if (variable_v < value) {
+            int start = 0;
+            while (variable_v < value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v++;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+    void increment_double_operatorLowerUpper(VariableDef& var, double value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        double variable_v = stringToDouble(var.VariableValue, "");
+
+        if (variable_v < value) {
+            int start = 0;
+            while (variable_v < value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v++;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+    void decrement_int_operatorLowerUpper(VariableDef& var, int value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        int variable_v = stringToInt(var.VariableValue, "");
+
+        if (variable_v < value) {
+            int start = 0;
+            while (variable_v < value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v--;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+    void decrement_double_operatorLowerUpper(VariableDef& var, double value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        double variable_v = stringToDouble(var.VariableValue, "");
+
+        if (variable_v < value) {
+            int start = 0;
+            while (variable_v < value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v--;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+
+
+
+
+    void increment_int_operatorUpperLower(VariableDef& var, int value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        int variable_v = stringToInt(var.VariableValue, "");
+
+        if (variable_v > value) {
+            int start = 0;
+            while (variable_v > value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v++;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+    void increment_double_operatorUpperLower(VariableDef& var, double value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        double variable_v = stringToDouble(var.VariableValue, "");
+
+        if (variable_v > value) {
+            int start = 0;
+            while (variable_v > value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v++;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+
+    void decrement_int_operatorUpperLower(VariableDef& var, int value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        int variable_v = stringToInt(var.VariableValue, "");
+
+        if (variable_v > value) {
+            int start = 0;
+            while (variable_v > value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v--;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+
+    void decrement_double_operatorUpperLower(VariableDef& var, double value, Data::Data& data, std::vector<Tokenizer::Token>& TantQue_Tokens, std::optional<FPL::FonctionDef>& fonction) {
+        double variable_v = stringToDouble(var.VariableValue, "");
+
+        if (variable_v > value) {
+            int start = 0;
+            while (variable_v > value) {
+                if (start == 0) {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                    start++;
+                } else {
+                    Parser::Parser::executeContentCode(TantQue_Tokens, fonction, std::nullopt, data);
+                }
+
+                variable_v--;
+                data.updateValue(var.VariableName, std::to_string(variable_v));
+            }
+        } // Si non est bien, on arrête l'instruction 'tant que'
+    }
+}
