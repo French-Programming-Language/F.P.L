5da97a021e0d32bf666964aceed47afd541a1b11
Program132 <clement.gauthier132@orange.fr>
2023-07-27 22:32:23 +0200

    V3 (Got Bug) + added "renvoyer"

--- src/Essentials/MathUtils/MathParser/Utils/Converts.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include <vector>
-#include <iostream>
-
-double convertDouble(std::string& n);
--- src/Essentials/Statement.cpp
+++
@@ -1,9 +0,0 @@
-#include "Statement.h"
-
-namespace FPL::Statement {
-    Statement::Statement(StatementKind kind, std::string name, FPL::Types::Types type) {
-        StatementType = type;
-        StatementName = name;
-        StatementKind{kind};
-    }
-}
